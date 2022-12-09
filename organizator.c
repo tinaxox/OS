1e083806965c9065c7d840b09d5d5a196690ee03
CodeDepo <codedepon@gmail.com>
2022-12-10 00:02:10 +0100

    klk2020

--- organizator.c
+++ organizator.c
@@ -16,6 +16,15 @@ typedef struct ucesnik
     int rbr;
 } ucesnik;
 
+typedef struct ponuda
+{
+    long mtype;
+    int iznos;
+    int rbr;
+    pid_t pid_ucesnika;
+
+} ponuda;
+
 int main()
 {
     // prijave
@@ -46,6 +55,7 @@ int main()
     // aukcije
     for (int i = 0; i < N; i++)
     {
+        ponuda ponude[M];
         pid_t aukcija = fork();
 
         if (aukcija == -1)
@@ -56,16 +66,62 @@ int main()
         if (aukcija == 0)
         {
             ucesnik ucesnik_na_aukciji[M];
-            for (int i = 0; i < M; i++)
+            for (int j = 0; j < M; j++)
             {
                 ucesnik a;
-                msgrcv(id_reda, (void *)&a, sizeof(a) - sizeof(a.mtype), i, 0);
-                ucesnik_na_aukciji[i] = a;
-                printf("%d %d na %d\n", ucesnik_na_aukciji[i].pid_ucesnika, ucesnik_na_aukciji[i].rbr, i);
+                msgrcv(id_reda, (void *)&a, sizeof(a) - sizeof(a.mtype), i + 1, 0);
+                ucesnik_na_aukciji[j] = a;
+                printf("%d %d na %d\n", ucesnik_na_aukciji[j].pid_ucesnika, ucesnik_na_aukciji[j].rbr, i);
             }
 
-            return 0;
+            printf("Svi su se prijavili.\n");
+            for (int k = 0; k < M; k++)
+            {
+                ucesnik u = {ucesnik_na_aukciji[k].pid_ucesnika, ucesnik_na_aukciji[k].pid_ucesnika, ucesnik_na_aukciji[k].rbr};
+
+                printf("MTYPE %ld\n", u.mtype);
+                msgsnd(id_reda, (void *)&u, sizeof(u) - sizeof(u.mtype), 0);
+            }
+            printf("AUKCIJA moze da pocne.\n");
+
+            ponuda max_aukcije;
+            for (int broj_kruga = 0; broj_kruga < 3; broj_kruga++)
+            {
+                ponuda max_runde;
+
+                for (int j = 0; j < M; j++)
+                {
+                    ponuda p;
+                    msgrcv(id_reda, (void *)&p, sizeof(p) - sizeof(p.mtype), i + 1, 0);
+                    printf("PONUDA: %d-%d %d\n", p.rbr, p.pid_ucesnika, p.iznos);
+                    if (j == 0 || p.iznos > max_runde.iznos)
+                    {
+                        max_runde = p;
+                    }
+                }
+                for (int j = 0; j < M; j++)
+                {
+                    max_runde.mtype = ucesnik_na_aukciji[j].pid_ucesnika; // moramo da menjamo mtype jer saljemo svakom, a ne samo max ponudi
+                    msgsnd(id_reda, (void *)&max_runde, sizeof(max_runde) - sizeof(max_runde.mtype), 0);
+                }
+                if (broj_kruga == 0 || max_runde.iznos > max_aukcije.iznos)
+                {
+                    max_aukcije = max_runde;
+                }
+            }
+            for (int j = 0; j < M; j++)
+            {
+                max_aukcije.mtype = ucesnik_na_aukciji[j].pid_ucesnika;
+                msgsnd(id_reda, (void *)&max_aukcije, sizeof(max_aukcije) - sizeof(max_aukcije.mtype), 0);
+            }
+
+            exit(0);
         }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        wait(NULL);
     }
 
     return 0;
--- ucesnik.c
+++ ucesnik.c
@@ -16,6 +16,15 @@ typedef struct ucesnik
     int rbr;
 } ucesnik;
 
+typedef struct ponuda
+{
+    long mtype;
+    int iznos;
+    int rbr;
+    pid_t pid_ucesnika;
+
+} ponuda;
+
 int main()
 {
     // prijave
@@ -32,11 +41,31 @@ int main()
     msgrcv(id_reda, (void *)&u, sizeof(u) - sizeof(u.mtype), u.pid_ucesnika, 0); // prima redni broj
     printf("ucesnik: %d sa rednim brojem %d ucestvovace na %d aukcija\n", u.pid_ucesnika, u.rbr, broj_aukcija);
 
-    for (int i = 0; i <)
-        // prijave na aukciju
-        int aukcija;
+    // prijave na aukciju
+    int aukcija;
     scanf("%d", &aukcija);
-    u.mtype = aukcija;
+    u.mtype = aukcija + 1;
     msgsnd(id_reda, (void *)&u, sizeof(u) - sizeof(u.mtype), 0);
+    printf("poslata prijava %ld\n", u.mtype);
+    printf("cekam: %d\n", u.pid_ucesnika);
+    msgrcv(id_reda, (void *)&u, sizeof(u) - sizeof(u.mtype), u.pid_ucesnika, 0); // ceka dok se ne prijave svi
+    // krece aukcija nakon sto on primi ovu poruku
+    printf("AUKCIJA moze da pocne.\n");
+
+    for (int broj_kruga = 0; broj_kruga < 3; broj_kruga++)
+    {
+        int iznos;
+        scanf("%d", &iznos);
+        ponuda p = {aukcija + 1, iznos, u.rbr, u.pid_ucesnika};
+
+        printf("broj kruga: %d\n", broj_kruga);
+        msgsnd(id_reda, (void *)&p, sizeof(p) - sizeof(p.mtype), 0);
+        ponuda max_runde;
+        msgrcv(id_reda, (void *)&max_runde, sizeof(max_runde) - sizeof(max_runde.mtype), u.pid_ucesnika, 0);
+    }
+    ponuda max;
+
+    msgrcv(id_reda, (void *)&max, sizeof(max) - sizeof(max.mtype), u.pid_ucesnika, 0);
+    printf("MAKSIMALNA PONUDA %d-%d %d\n", max.rbr, max.pid_ucesnika, max.iznos);
     return 0;
 }
